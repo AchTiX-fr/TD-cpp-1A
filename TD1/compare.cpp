6607ee10bad0a84cf2fbb58dc467a4cabf4be7ed
AchTiX-fr <achtix42@gmail.com>
2026-03-19 23:26:47 +0100

    Delete TD1 directory

--- TD1/compare.cpp
+++
@@ -1,6 +0,0 @@
-#include "compare.h"
-
-bool areEqual(int nb_1, int nb_2)
-{
-	return nb_1 == nb_2;
-}
--- TD1/compare.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-
-/**
-* Compare two int
-* @param value : two int
-* @returns Return 1 if the values are equal, else 0
-**/
-bool areEqual(int, int);
